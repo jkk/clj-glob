0bf8742594c96b3cd285cf910a20d3d14b245bcc
Justin Kramer <jkkramer@gmail.com>
2010-11-07 22:48:05 -0500

    indentation

--- test/org/satta/glob_test.clj
+++ test/org/satta/glob_test.clj
@@ -15,43 +15,43 @@
 (deftest test-glob->regex
   (testing "General matching"
     (are [pattern path] (matches? pattern path)
-         "abcd"        "abcd"
-         "a*d"         "abcd"
-         "a??d"        "abcd"
-         "*"           "abcd"
-         "foo.bar"     "foo.bar"
-         "foo.*"       "foo.bar"
-         "*.bar"       "foo.bar"
-         "*foo.*"      "foo.bar"
-         "foo*"        "foo.bar"
-         "*.{bar,baz}" "foo.bar"
-         "*.{bar,baz}" "foo.baz"
-         "{foo,bar}"   "foo"
-         "{foo,bar}"   "bar"
-         "foo,bar"     "foo,bar"
-         "*,*"         "foo,bar"
-         "foo\\*bar"   "foo*bar"
-         ".()|+^$@%"   ".()|+^$@%"
-         ".*.foo"      ".bar.foo"
-         ".*bar.foo"   ".bar.foo"
-         "foo.[ch]"    "foo.c"
-         "foo.[ch]"    "foo.h"
-         "foo.[c-h]"   "foo.c"
-         "foo.[c-h]"   "foo.e"
-         "foo.[c-h]"   "foo.h"))
+      "abcd"        "abcd"
+      "a*d"         "abcd"
+      "a??d"        "abcd"
+      "*"           "abcd"
+      "foo.bar"     "foo.bar"
+      "foo.*"       "foo.bar"
+      "*.bar"       "foo.bar"
+      "*foo.*"      "foo.bar"
+      "foo*"        "foo.bar"
+      "*.{bar,baz}" "foo.bar"
+      "*.{bar,baz}" "foo.baz"
+      "{foo,bar}"   "foo"
+      "{foo,bar}"   "bar"
+      "foo,bar"     "foo,bar"
+      "*,*"         "foo,bar"
+      "foo\\*bar"   "foo*bar"
+      ".()|+^$@%"   ".()|+^$@%"
+      ".*.foo"      ".bar.foo"
+      ".*bar.foo"   ".bar.foo"
+      "foo.[ch]"    "foo.c"
+      "foo.[ch]"    "foo.h"
+      "foo.[c-h]"   "foo.c"
+      "foo.[c-h]"   "foo.e"
+      "foo.[c-h]"   "foo.h"))
   (testing "Dot files ignored"
     (are [pattern path] (not (matches? pattern path))
-         "*"           ".foo"
-         "*.*"         ".foo"
-         "*.foo"       ".bar.foo"
-         "*.bar.foo"   ".bar.foo"
-         "?bar.foo"    ".bar.foo"))
+      "*"           ".foo"
+      "*.*"         ".foo"
+      "*.foo"       ".bar.foo"
+      "*.bar.foo"   ".bar.foo"
+      "?bar.foo"    ".bar.foo"))
   (testing "Character ranges that shouldn't match"
     (are [pattern path] (not (matches? pattern path))
-         "foo.[ch]"    "foo.a"
-         "foo.[ch]"    "foo.d"
-         "foo.[c-h]"   "foo.b"
-         "foo.[c-h]"   "foo.i")))
+      "foo.[ch]"    "foo.a"
+      "foo.[ch]"    "foo.d"
+      "foo.[c-h]"   "foo.b"
+      "foo.[c-h]"   "foo.i")))
 
 ;; glob ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -75,18 +75,18 @@
            ["foo" "bar" "baz"]))))
 
 (def shallow-fs
-     (mock-fs
-      ["." "cat.jpg" "dog.jpg" "lol.gif" ".hidden"]))
+  (mock-fs
+   ["." "cat.jpg" "dog.jpg" "lol.gif" ".hidden"]))
 
 (def deep-fs
-     (mock-fs
-      ["/"
-       ["usr"
-        ["bin" "awk" "bzip" "cd" "diff" "sed" "segedit" "xargs"]
-        ["lib" "pam" "samba" "java"]
-        ["sbin" "arp" "fdisk" "sendmail" "tcpdump"]
-        ["share"
-         ["man" "man1" "man2" "man3" ".hidden"]]]]))
+  (mock-fs
+   ["/"
+    ["usr"
+     ["bin" "awk" "bzip" "cd" "diff" "sed" "segedit" "xargs"]
+     ["lib" "pam" "samba" "java"]
+     ["sbin" "arp" "fdisk" "sendmail" "tcpdump"]
+     ["share"
+      ["man" "man1" "man2" "man3" ".hidden"]]]]))
 
 (defn glob*
   "Glob with a fake filesystem. Also returns seq of file names rather
@@ -99,16 +99,16 @@
 (deftest test-glob
   (testing "Simple, shallow (single-level) matching"
     (are [pattern files] (= (glob* pattern shallow-fs) files)
-         "*"            ["cat.jpg" "dog.jpg" "lol.gif"]
-         "*.*"          ["cat.jpg" "dog.jpg" "lol.gif"]
-         ".*"           [".hidden"]
-         "*.jpg"        ["cat.jpg" "dog.jpg"]
-         "*.{jpg,gif}"  ["cat.jpg" "dog.jpg" "lol.gif"]))
+      "*"            ["cat.jpg" "dog.jpg" "lol.gif"]
+      "*.*"          ["cat.jpg" "dog.jpg" "lol.gif"]
+      ".*"           [".hidden"]
+      "*.jpg"        ["cat.jpg" "dog.jpg"]
+      "*.{jpg,gif}"  ["cat.jpg" "dog.jpg" "lol.gif"]))
   (testing "Deep (multi-level) matching"
     (are [pattern files] (= (glob* pattern deep-fs) files)
-         "/*"         ["usr"]
-         "/usr/*"     ["bin" "lib" "sbin" "share"]
-         "/usr/*/se*" ["sed" "segedit" "sendmail"]
-         "/*/*/a*"    ["awk" "arp"]
-         "/*/*/*/*"   ["man1" "man2" "man3"]
-         "/*/*/*/.*"  [".hidden"])))
+      "/*"         ["usr"]
+      "/usr/*"     ["bin" "lib" "sbin" "share"]
+      "/usr/*/se*" ["sed" "segedit" "sendmail"]
+      "/*/*/a*"    ["awk" "arp"]
+      "/*/*/*/*"   ["man1" "man2" "man3"]
+      "/*/*/*/.*"  [".hidden"])))
